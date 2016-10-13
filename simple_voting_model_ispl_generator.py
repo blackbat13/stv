08dcb350554c94f000cd414624ffebac1f30058a
blackbat13 <blackbat13@gmail.com>
2016-10-13 14:28:55 +0200

    Finished simple voting model ispl generator

--- simple_voting_model_ispl_generator.py
+++ simple_voting_model_ispl_generator.py
@@ -10,6 +10,7 @@ class SimpleVotingModelIsplGenerator:
         self.number_of_voters = number_of_voters
 
     def create_ispl_model(self):
+        self.ispl_model += "Semantics = SA;\n"
         self.ispl_model += self.__create_environment()
         self.ispl_model += self.__create_coercer()
         for voter_number in range(1, self.number_of_voters + 1):
@@ -23,7 +24,7 @@ class SimpleVotingModelIsplGenerator:
     def __create_environment(self):
         environment = "Agent Environment\n"
         environment += self.__create_environment_obsvars()
-        # environment += self.__create_environment_vars()
+        environment += self.__create_environment_vars()
         environment += self.__create_environment_actions()
         environment += self.__create_environment_protocol()
         environment += self.__create_environment_evolution()
@@ -32,14 +33,20 @@ class SimpleVotingModelIsplGenerator:
 
     def __create_environment_obsvars(self):
         obsvars = "\tObsvars:\n"
-        for voter_number in range(1, self.number_of_voters + 1):
-            obsvars += "\t\tvoter" + str(voter_number) + "Voted: boolean;\n"
+
         obsvars += "\tend Obsvars\n"
         return obsvars
 
     def __create_environment_vars(self):
         vars = "\tVars:\n"
-
+        # for voter_number in range(1, self.number_of_voters + 1):
+        #     vars += "\t\tvoter" + str(voter_number) + "Vote: 0.." + str(self.number_of_candidates) + ";\n"
+        for voter_number in range(1, self.number_of_voters + 1):
+            vars += "\t\tvoter" + str(voter_number) + "Voted: boolean;\n"
+        for voter_number in range(1, self.number_of_voters + 1):
+            vars += "\t\tvoter" + str(voter_number) + "Decided: boolean;\n"
+        for voter_number in range(1, self.number_of_voters + 1):
+            vars += "\t\tvoter" + str(voter_number) + "Pun: {pun, np, None};\n"
         vars += "\tend Vars\n"
         return vars
 
@@ -53,13 +60,27 @@ class SimpleVotingModelIsplGenerator:
 
     def __create_environment_evolution(self):
         evolution = "\tEvolution:\n"
-
+        for voter_number in range(1, self.number_of_voters + 1):
+            evolution += "\t\tvoter" + str(voter_number) + "Voted=true if\n"
+            for candidate_number in range(1, self.number_of_candidates+1):
+                evolution += "\t\t\tVoter" + str(voter_number) + ".Action=Vote" + str(candidate_number) + " or\n"
+            evolution = evolution.rstrip("\nro ")
+            evolution += ";\n"
+        for voter_number in range(1, self.number_of_voters + 1):
+            evolution += "\t\tvoter" + str(voter_number) + "Decided=true if\n"
+            evolution += "\t\t\tVoter"+ str(voter_number) + ".Action=Give or\n"
+            evolution += "\t\t\tVoter" + str(voter_number) + ".Action=Ng;\n"
+            evolution += "\t\tvoter" + str(voter_number) + "Pun=pun if\n"
+            evolution += "\t\t\tCoercer.Action=Pun" + str(voter_number) + ";\n"
+            evolution += "\t\tvoter" + str(voter_number) + "Pun=np if\n"
+            evolution += "\t\t\tCoercer.Action=Np" + str(voter_number) + ";\n"
         evolution += "\tend Evolution\n"
         return evolution
 
     def __create_coercer(self):
         player = "Agent Coercer\n"
 
+        player += self.__create_coercer_lobsvars()
         player += self.__create_coercer_vars()
         player += self.__create_coercer_actions()
         player += self.__create_coercer_protocol()
@@ -69,38 +90,46 @@ class SimpleVotingModelIsplGenerator:
 
     def __create_coercer_lobsvars(self):
         lobsvars = "\tLobsvars = {"
+        for voter_number in range(1, self.number_of_voters + 1):
+            lobsvars += "voter" + str(voter_number) + "Voted, "
+            lobsvars += "voter" + str(voter_number) + "Decided, "
+            lobsvars += "voter" + str(voter_number) + "Pun, "
 
+        lobsvars = lobsvars.rstrip(" ,")
         lobsvars += "};\n"
         return lobsvars
 
     def __create_coercer_vars(self):
         vars = "\tVars:\n"
-
+        vars += "\t\tx: boolean;\n"
         vars += "\tend Vars\n"
         return vars
 
     def __create_coercer_actions(self):
         actions = "\tActions = {"
-
+        for voter_number in range(1, self.number_of_voters + 1):
+            actions += "Pun" + str(voter_number) + ", Np" + str(voter_number) + ", "
         actions += "Wait};\n"
         return actions
 
     def __create_coercer_protocol(self):
         protocol = "\tProtocol:\n"
-
+        for voter_number in range(1, self.number_of_voters + 1):
+            protocol += "\t\tEnvironment.voter" + str(voter_number) + "Voted=true and Environment.voter" + str(voter_number) + "Pun=None and Environment.voter"+ str(voter_number) + "Decided=true: {Pun" + str(voter_number) + ", Np" + str(voter_number) + ", Wait};\n"
         protocol += "\t\tOther: {Wait};\n"
         protocol += "\tend Protocol\n"
         return protocol
 
     def __create_coercer_evolution(self, ):
         evolution = "\tEvolution:\n"
-
+        evolution += "\t\tx=false if Action=Wait;\n"
         evolution += "\tend Evolution\n"
         return evolution
 
     def __create_voter(self, voter_number):
         player = "Agent Voter" + str(voter_number) + "\n"
 
+        player += self.__create_voter_lobsvars(voter_number)
         player += self.__create_voter_vars()
         player += self.__create_voter_actions()
         player += self.__create_voter_protocol()
@@ -108,9 +137,9 @@ class SimpleVotingModelIsplGenerator:
         player += "end Agent\n\n"
         return player
 
-    def __create_voter_lobsvars(self):
+    def __create_voter_lobsvars(self, voter_number):
         lobsvars = "\tLobsvars = {"
-
+        lobsvars += "voter" + str(voter_number) + "Voted, voter" + str(voter_number) + "Pun, voter" + str(voter_number) + "Decided"
         lobsvars += "};\n"
         return lobsvars
 
@@ -152,7 +181,10 @@ class SimpleVotingModelIsplGenerator:
 
     def __create_evaluation(self):
         evaulation = "Evaluation\n"
-
+        evaulation += "\tVoter1Punished if Environment.voter1Pun=pun;\n"
+        evaulation += "\tVoter1NotPunished if Environment.voter1Pun=np;\n"
+        evaulation += "\tVoter1Voted1 if Voter1.vote=1;\n"
+        evaulation += "\tVoter1Finish if Environment.voter1Pun=pun or Environment.voter1Pun=np;\n"
         evaulation += "end Evaluation\n\n"
         return evaulation
 
@@ -160,7 +192,8 @@ class SimpleVotingModelIsplGenerator:
         init_states = "InitStates\n"
         init_states += "\t\t"
         for voter_number in range(1, self.number_of_voters + 1):
-            init_states += "Voter" + str(voter_number) + ".vote=0 and Voter" + str(voter_number) + ".decision=None and"
+            init_states += "Voter" + str(voter_number) + ".vote=0 and Voter" + str(voter_number) + ".decision=None and Environment.voter" + str(voter_number) + "Voted=false and Environment.voter" + str(voter_number) + "Pun=None and Environment.voter" + str(voter_number) + "Decided=false and "
+        init_states += "Coercer.x=false"
         init_states += ";\n"
         init_states += "end InitStates\n\n"
         return init_states
@@ -175,13 +208,16 @@ class SimpleVotingModelIsplGenerator:
 
     def __create_formulae(self):
         formulae = "Formulae\n"
-
+        formulae += "\t<c>F(!Voter1Punished -> Voter1Voted1);\n"
+        formulae += "\t<v1>G(!Voter1Punished and !Voter1Voted1);\n"
+        formulae += "\t<c>G((Voter1Finish and !Voter1Punished) -> Voter1Voted1);\n"
+        formulae += "\t<v1>F(Voter1Finish and !Voter1Punished and !Voter1Voted1);\n"
         formulae += "end Formulae\n\n"
         return formulae
 
 
 n = 2
-k = 1
+k = 3
 simple_voting_model_ispl_generator = SimpleVotingModelIsplGenerator(n, k)
 f = open("simple_voting_" + str(n) + "_" + str(k) + ".ispl", "w")
 f.write(simple_voting_model_ispl_generator.create_ispl_model())
