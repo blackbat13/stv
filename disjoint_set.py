273ce738ddcfb2e0daf875513ca0413bab994bf2
blackbat13 <blackbat13@gmail.com>
2016-10-27 16:16:30 +0200

    Added checking if nodes are in same set

--- disjoint_set.py
+++ disjoint_set.py
@@ -2,23 +2,27 @@ class DisjointSet:
     subsets = []
 
     def __init__(self, number_of_nodes):
+        self.subsets = []
         for i in range(0, number_of_nodes):
             self.subsets.append({'parent': i, 'rank': 0})
 
     def find(self, node_number):
-        if self.subsets[node_number].parent != node_number:
-            self.subsets[node_number] = self.find(self.subsets[node_number].parent)
+        if self.subsets[node_number]['parent'] != node_number:
+            self.subsets[node_number]['parent'] = self.find(self.subsets[node_number]['parent'])
 
-        return self.subsets[node_number].parent
+        return self.subsets[node_number]['parent']
 
     def union(self, x, y):
-        xRoot = self.find(x)
-        yRoot = self.find(y)
+        x_root = self.find(x)
+        y_root = self.find(y)
 
-        if self.subsets[xRoot].rank < self.subsets[yRoot].rank:
-            self.subsets[xRoot].parent = yRoot
-        elif self.subsets[xRoot].rank > self.subsets[yRoot].rank:
-            self.subsets[yRoot].parent = xRoot
+        if self.subsets[x_root]['rank'] < self.subsets[y_root]['rank']:
+            self.subsets[x_root]['parent'] = y_root
+        elif self.subsets[x_root]['rank'] > self.subsets[y_root]['rank']:
+            self.subsets[y_root]['parent'] = x_root
         else:
-            self.subsets[yRoot].parent = xRoot
-            self.subsets[xRoot].rank += 1
+            self.subsets[y_root]['parent'] = x_root
+            self.subsets[x_root]['rank'] += 1
+
+    def is_same(self, x, y):
+        return self.find(x) == self.find(y)
