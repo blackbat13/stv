090b1dc5786130f7a90cab2673a93d9be4eb2246
blackbat13 <blackbat13@gmail.com>
2018-03-05 14:50:30 +0100

    Added array tools

---
+++ tools/array_tools.py
@@ -0,0 +1,20 @@
+class ArrayTools:
+    @staticmethod
+    def create_array_of_size(size: int, basic_item) -> list:
+        """Creates array of given size containing copies of given items"""
+        array = []
+        for i in range(0, size):
+            array.append(basic_item.copy())
+        return array[:]
+
+    @staticmethod
+    def unique(l: list):
+        """Removes all duplicates from list"""
+        s = set()
+        n = 0
+        for x in l:
+            if x not in s:
+                s.add(x)
+                l[n] = x
+                n += 1
+        del l[n:]
