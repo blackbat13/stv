dd4b9b9d2f0e2f41970ebcf275c79876af680a56
blackbat13 <blackbat13@gmail.com>
2018-03-05 15:05:03 +0100

    Added tests for array tools

---
+++ tests/tools/test_array_tools.py
@@ -0,0 +1,18 @@
+import unittest
+from tools.array_tools import ArrayTools
+
+
+class TestArrayTools(unittest.TestCase):
+    def test_create_array_of_size(self):
+        array = ArrayTools.create_array_of_size(10, [])
+        self.assertEqual(len(array), 10)
+        self.assertEqual(len(array[0]), 0)
+
+    def test_unique(self):
+        array = [1,2,3,1,2,3]
+        ArrayTools.unique(array)
+        self.assertEqual(len(array), 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
