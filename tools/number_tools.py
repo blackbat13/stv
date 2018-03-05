4f7f3c866bbab1f2b7261d3ad7e3ef967c55a7c0
blackbat13 <blackbat13@gmail.com>
2018-03-05 14:52:32 +0100

    Added number tools

---
+++ tools/number_tools.py
@@ -0,0 +1,14 @@
+class NumberTools:
+    @staticmethod
+    def max(a: int, b: int) -> int:
+        if a > b:
+            return a
+        else:
+            return b
+
+    @staticmethod
+    def min(a: int, b: int) -> int:
+        if a < b:
+            return a
+        else:
+            return b
