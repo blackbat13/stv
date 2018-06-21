6bd9fc7d0429251294de30926223561d9c833e12
blackbat13 <blackbat13@gmail.com>
2018-06-21 13:04:12 +0200

    Added basic version of machine model generator

--- comparing_strats/graph_drawing.py
+++ comparing_strats/graph_drawing.py
@@ -20,6 +20,8 @@ class GraphDrawing:
                 label += f'{key}:{state[key]}\n'
             if self.strategy[i] is not None:
                 graph.add_node(i, label=label, color='green')
+            elif i == 0:
+                graph.add_node(i, label=label, color='blue')
             else:
                 graph.add_node(i, label=label, color='red')
 
@@ -33,13 +35,14 @@ class GraphDrawing:
                     graph.add_edges_from([(state, next_state)],
                                          label=transition['actions'], color='red')
 
-        plt.figure(1, figsize=(50, 10))
+        plt.figure(1, figsize=(40, 40))
         plt.subplot(111)
-        pos = networkx.drawing.nx_agraph.graphviz_layout(graph, prog='dot', root=0)
+        # neato dot fdp sfdp twopi circo
+        pos = networkx.drawing.nx_agraph.graphviz_layout(graph, prog='neato', root=0)
 
         labels = networkx.get_edge_attributes(graph, 'label')
-        networkx.draw(graph, pos=pos, with_labels=False, font_weight='bold', node_color=list(networkx.get_node_attributes(graph, 'color').values()), edge_color=list(networkx.get_edge_attributes(graph, 'color').values()))
-        networkx.draw_networkx_edge_labels(graph, pos=pos, edge_labels=labels)
+        networkx.draw(graph, pos=pos, node_size=16500, with_labels=False, font_weight='bold', node_color=list(networkx.get_node_attributes(graph, 'color').values()), edge_color=list(networkx.get_edge_attributes(graph, 'color').values()), arrows=True, arrowsize=30)
+        networkx.draw_networkx_edge_labels(graph, pos=pos, edge_labels=labels, label_pos=0.4)
         networkx.draw_networkx_labels(graph, pos=pos, labels=networkx.get_node_attributes(graph, 'label'))
         plt.autoscale()
         plt.show()
---
+++ machines_model/machines_model.py
@@ -0,0 +1,179 @@
+from comparing_strats.simple_model import SimpleModel
+import itertools
+from disjoint_set import DisjointSet
+from random import randint
+from typing import List
+from comparing_strats.graph_drawing import GraphDrawing
+
+
+class MachineModel:
+    model = None
+    states = []
+    states_dictionary = {}
+    state_number = 0
+    no_robots = 0
+    no_machines = 0
+    map = []
+    machine_requirements = []
+    items_limit = 0
+
+    def __init__(self, no_robots: int, no_machines: int, map_size: int, items_limit: int):
+        self.no_robots = no_robots
+        self.no_machines = no_machines
+        self.items_limit = items_limit
+        self.map = []
+        for i in range(0, map_size):
+            self.map.append([])
+            for j in range(0, map_size):
+                self.map[i].append(0)
+
+        for i in range(0, no_robots):
+            self.map[0][i] = 1
+
+        for i in range(0, no_machines):
+            self.map[-1][i] = no_machines
+
+        self.machine_requirements.append([0, 1])
+        self.machine_requirements.append([0, 0])
+        self.model = SimpleModel(no_robots + no_machines)
+        self.generate_model()
+        self.model.states = self.states
+
+    def generate_model(self):
+        first_state = {'r_pos': [(0, 0)],
+                       'm_pos': [(len(self.map) - 1, 0), (len(self.map) - 1, 1)],
+                       'r_items': [-1], 'm_in': [[0, 0], [0, 0]], 'm_out': [0, 0],
+                       'it_count': [0, 0]}
+
+        self.add_state(first_state)
+
+        current_state_number = -1
+        for state in self.states:
+            current_state_number += 1
+            available_actions = []
+
+            is_end_state = True
+
+            for i in range(0, self.no_machines):
+                if state['it_count'][i] < self.items_limit:
+                    is_end_state = False
+                    break
+
+            for i in range(0, self.no_robots):
+                robot_item = state['r_items'][i]
+                robot_position = state['r_pos'][i]
+                available_actions.append([])
+                if is_end_state:
+                    available_actions[-1].append(('Wait', 0))
+                    continue
+
+                if robot_position[0] > 0:
+                    available_actions[-1].append(('E', (robot_position[0] - 1, robot_position[1])))
+                if robot_position[0] < len(self.map) - 1:
+                    available_actions[-1].append(('W', (robot_position[0] + 1, robot_position[1])))
+                if robot_position[1] > 0:
+                    available_actions[-1].append(('N', (robot_position[0], robot_position[1] - 1)))
+                if robot_position[1] < len(self.map) - 1:
+                    available_actions[-1].append(('S', (robot_position[0], robot_position[1] + 1)))
+
+                machine_number = -1
+                for machine_position in state['m_pos']:
+                    machine_number += 1
+                    if robot_position[0] == machine_position[0] and robot_position[1] == machine_position[1]:
+                        if robot_item != -1 and self.machine_requirements[machine_number][robot_item] > 0:
+                            available_actions[-1].append(('leave', machine_number))
+
+                        if robot_item == -1 and state['m_out'][machine_number] > 0:
+                            available_actions[-1].append(('pick', machine_number))
+
+                available_actions[-1].append('wait')
+
+            for i in range(0, self.no_machines):
+                available_actions.append([])
+                machine_input = state['m_in'][i]
+                met_requirements = True
+                for j in range(0, len(machine_input)):
+                    if machine_input[j] < self.machine_requirements[i][j]:
+                        met_requirements = False
+                        break
+
+                if state['it_count'][i] >= self.items_limit:
+                    met_requirements = False
+
+                if met_requirements:
+                    available_actions[-1].append(('produce', i))
+                else:
+                    available_actions[-1].append(('Wait', 0))
+
+            for current_actions in itertools.product(*available_actions):
+                robot_positions = state['r_pos'][:]
+                machine_positions = state['m_pos'][:]
+                robot_items = state['r_items'][:]
+                machine_inputs = []
+                for i in range(0, self.no_machines):
+                    machine_inputs.append(state['m_in'][i][:])
+
+                machine_outputs = state['m_out'][:]
+                produced_items_count = state['it_count'][:]
+
+                actions = []
+
+                for robot_number in range(0, self.no_robots):
+                    robot_action = current_actions[robot_number]
+                    actions.append(robot_action[0])
+                    if robot_action[0] in ['N', 'W', 'S', 'E']:
+                        robot_positions[robot_number] = (robot_action[1][0], robot_action[1][1])
+
+                    if robot_action[0] == 'leave':
+                        machine_inputs[robot_action[1]][robot_items[robot_number]] += 1
+                        robot_items[robot_number] = -1
+
+                    if robot_action[0] == 'pick':
+                        machine_outputs[robot_action[1]] -= 1
+                        robot_items[robot_number] = robot_action[1]
+
+                for machine_number in range(0, self.no_machines):
+                    machine_action = current_actions[self.no_robots + machine_number]
+                    actions.append(machine_action[0])
+                    if machine_action[0] == 'produce':
+                        for i in range(0, len(self.machine_requirements[machine_number])):
+                            machine_inputs[machine_number][i] -= self.machine_requirements[machine_number][i]
+
+                        machine_outputs[machine_number] += 1
+                        produced_items_count[machine_number] += 1
+
+                new_state = {'r_pos': robot_positions, 'm_pos': machine_positions,
+                             'r_items': robot_items, 'm_in': machine_inputs,
+                             'm_out': machine_outputs, 'it_count': produced_items_count}
+
+                new_state_number = self.add_state(new_state)
+                self.model.add_transition(current_state_number, new_state_number, actions)
+
+    def add_state(self, state: hash) -> int:
+        new_state_number = self.get_state_number(state)
+        # epistemic_state = self.get_epistemic_state(state)
+        # self.add_to_epistemic_dictionary(epistemic_state, new_state_number)
+        return new_state_number
+
+    def get_state_number(self, state: hash) -> int:
+        state_str = ' '.join(str(state[e]) for e in state)
+        if state_str not in self.states_dictionary:
+            self.states_dictionary[state_str] = self.state_number
+            new_state_number = self.state_number
+            self.states.append(state)
+            self.state_number += 1
+        else:
+            new_state_number = self.states_dictionary[state_str]
+
+        return new_state_number
+
+
+machine_model = MachineModel(no_robots=1, no_machines=2, map_size=2, items_limit=1)
+print(f'Number of states: {len(machine_model.states)}')
+strategy = []
+for state in machine_model.states:
+    print(state)
+    strategy.append(None)
+
+graphDrawing = GraphDrawing(machine_model.model, strategy)
+graphDrawing.draw()
