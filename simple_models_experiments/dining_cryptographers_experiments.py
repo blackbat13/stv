34afadeba8a9d229f51fe6f87f7313398d16bedd
blackbat13 <blackbat13@gmail.com>
2018-10-04 15:47:25 +0200

    Added dining cryptographers model

---
+++ simple_models_experiments/dining_cryptographers_experiments.py
@@ -0,0 +1,13 @@
+from simple_models.dining_cryptographers import DiningCryptographers
+from comparing_strats.graph_drawing import GraphDrawing
+
+diningCryptographers = DiningCryptographers(3)
+model = diningCryptographers.model
+strategy = []
+
+for i in range(0, len(model.states)):
+    strategy.append(None)
+
+
+graphDrawing = GraphDrawing(model, strategy)
+graphDrawing.draw()
