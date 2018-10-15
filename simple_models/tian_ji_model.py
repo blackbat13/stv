9a8b4b446e8a94b5244357c2ab857f241a85858a
blackbat13 <blackbat13@gmail.com>
2018-10-16 01:00:47 +0200

    Fixed Tian Ji model and added simple experiments

--- simple_models/tian_ji_model.py
+++ simple_models/tian_ji_model.py
@@ -22,7 +22,8 @@ class TianJiModel:
         king_horses = list(range(0, self.no_horses))
         tian_ji_horses = list(range(0, self.no_horses))
         first_state = {'king_score': 0, 'tian_ji_score': 0, 'king_horses': king_horses,
-                       'tian_ji_horses': tian_ji_horses, 'results': ArrayTools.create_array_of_size(self.no_horses, 0)}
+                       'tian_ji_horses': tian_ji_horses,
+                       'results': ArrayTools.create_value_array_of_size(self.no_horses, 0), 'king_choice': -1}
         return first_state
 
     def add_state(self, state: hash) -> int:
@@ -45,7 +46,7 @@ class TianJiModel:
 
     def get_epistemic_state(self, state: hash) -> hash:
         epistemic_state = {'king_score': state['king_score'], 'tian_ji_score': state['tian_ji_score'],
-                           'tian_ji_horses': state['tian_ji_horses']}
+                           'tian_ji_horses': state['tian_ji_horses'], 'king_choice': state['king_choice']}
         return epistemic_state
 
     def add_to_epistemic_dictionary(self, state: hash, new_state_number: int):
@@ -64,12 +65,16 @@ class TianJiModel:
             if len(state['king_horses']) == 0:
                 continue
 
+            if len(state['king_horses']) < len(state['tian_ji_horses']):
+                continue
+
             for king_horse in state['king_horses']:
                 new_king_horses = state['king_horses'][:]
                 new_king_horses.remove(king_horse)
                 new_king_state = {'king_score': state['king_score'], 'tian_ji_score': state['tian_ji_score'],
                                   'king_horses': new_king_horses,
-                                  'tian_ji_horses': state['tian_ji_horses'], 'results': state['results']}
+                                  'tian_ji_horses': state['tian_ji_horses'], 'results': state['results'],
+                                  'king_choice': king_horse}
                 king_actions = ['Wait', f'Send{king_horse}']
                 new_king_state_number = self.add_state(new_king_state)
                 self.model.add_transition(current_state_number, new_king_state_number, king_actions)
@@ -88,7 +93,7 @@ class TianJiModel:
 
                     new_state = {'king_score': new_king_score, 'tian_ji_score': new_tian_ji_score,
                                  'king_horses': new_king_horses,
-                                 'tian_ji_horses': state['tian_ji_horses'], 'results': new_results}
+                                 'tian_ji_horses': new_tian_ji_horses, 'results': new_results, 'king_choice': -1}
                     actions = [f'Send{tian_ji_horse}', 'Wait']
                     new_state_number = self.add_state(new_state)
                     self.model.add_transition(new_king_state_number, new_state_number, actions)
