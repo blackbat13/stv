407cf7ccdca4d1bffbdfaaacf4061924bbee699b
blackbat <blackbat13@gmail.com>
2018-11-13 16:35:30 +0100

    Added result parser

---
+++ result_parser/bridge_dfs_results_parser.py
@@ -0,0 +1,15 @@
+n = 4
+results_file = open(f"strat_dfs_bridge_results_{n}.txt", "r")
+result_sum = 0
+for _ in range(0, 50):
+    for _ in range(0, 6):
+        results_file.readline()
+
+    str = results_file.readline()
+    results_file.readline()
+    results_file.readline()
+    str = str.split(':')[1]
+    str = str.rstrip(' seconds\n')
+    result_sum += float(str)
+
+print(f'Average={result_sum/50.0} seconds')
