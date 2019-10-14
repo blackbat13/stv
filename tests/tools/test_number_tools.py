1701ba981b4378d4a6c027d7f5ad988c6bd3a043
blackbat13 <blackbat13@gmail.com>
2019-10-14 12:03:22 +0200

    Refactored tools tests

--- tests/tools/test_number_tools.py
+++ tests/tools/test_number_tools.py
@@ -1,15 +1,15 @@
 import unittest
-from tools.number_tools import NumberTools
+from ..context import tools
 
 
-class TestNumberTools(unittest.TestCase):
+class NumberToolsTestSuite(unittest.TestCase):
     def test_max(self):
-        self.assertEqual(NumberTools.max(10, 5), 10)
-        self.assertEqual(NumberTools.max(5, 10), 10)
+        self.assertEqual(tools.NumberTools.max(10, 5), 10)
+        self.assertEqual(tools.NumberTools.max(5, 10), 10)
 
     def test_min(self):
-        self.assertEqual(NumberTools.min(10, 5), 5)
-        self.assertEqual(NumberTools.min(5, 10), 5)
+        self.assertEqual(tools.NumberTools.min(10, 5), 5)
+        self.assertEqual(tools.NumberTools.min(5, 10), 5)
 
 
 if __name__ == '__main__':
