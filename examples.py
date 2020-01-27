f03eb09896aebae5da1c979e1b3d19245c83a506
blackbat <blackbat13@gmail.com>
2020-01-27 11:03:51 +0100

    Small fixes

--- examples.py
+++ examples.py
@@ -92,10 +92,10 @@
 #     random_model_exp = DroneModelExp(model_size=model_size, exp_count=10, filename=f"drone_results_{model_size}_v4.txt", timeout=60, energy=model_size * 2)
 #     random_model_exp.run_experiments()
 
-from experiments.strategy_simpl.drone_model_exp2 import DroneModelExp2
-
-# data = [10, 100, 1000, 10000, 100000]
-data = [3, 5]
-for model_size in data:
-    random_model_exp = DroneModelExp2(model_size=model_size, exp_count=5, filename=f"drone2_results_{model_size}_v1.txt", timeout=60, energy=model_size*2)
-    random_model_exp.run_experiments()
+# from experiments.strategy_simpl.drone_model_exp2 import DroneModelExp2
+#
+# # data = [10, 100, 1000, 10000, 100000]
+# data = [3, 5]
+# for model_size in data:
+#     random_model_exp = DroneModelExp2(model_size=model_size, exp_count=5, filename=f"drone2_results_{model_size}_v1.txt", timeout=60, energy=model_size*2)
+#     random_model_exp.run_experiments()
