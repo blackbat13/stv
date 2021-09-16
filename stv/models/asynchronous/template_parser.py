7561c1d44353512dcf5007aba1aa621465c8cd01
blackbat13 <blackbat13@gmail.com>
2021-09-16 09:56:58 +0200

    Added simple voting model template

--- stv/models/asynchronous/template_parser.py
+++ stv/models/asynchronous/template_parser.py
@@ -10,7 +10,7 @@ def train_controller():
     input_filename = "train_controller.mako"
     config = {"N_Trains": 2,
               "N_Controllers": 1}
-    output_filename = f"train_controller_{config['N_Trains']}_{config['N_Controllers']}.txt"
+    output_filename = f"train_controller_{config['N_Trains']}t_{config['N_Controllers']}c.txt"
 
     return input_filename, output_filename, config
 
@@ -19,7 +19,16 @@ def train_controller_synchronous():
     input_filename = "train_controller_synchronous.mako"
     config = {"N_Trains": 2,
               "N_Controllers": 1}
-    output_filename = f"train_controller_synchronous_{config['N_Trains']}_{config['N_Controllers']}.txt"
+    output_filename = f"train_controller_synchronous_{config['N_Trains']}t_{config['N_Controllers']}c.txt"
+
+    return input_filename, output_filename, config
+
+
+def simple_voting():
+    input_filename = "simple_voting.mako"
+    config = {"N_Voters": 1,
+              "N_Candidates": 2}
+    output_filename = f"simple_voting_{config['N_Voters']}v_{config['N_Candidates']}c.txt"
 
     return input_filename, output_filename, config
 
@@ -28,7 +37,7 @@ if __name__ == "__main__":
     input_path = "specs/templates/"
     output_path = "specs/generated/"
 
-    input_filename, output_filename, config = train_controller_synchronous()
+    input_filename, output_filename, config = train_controller()
 
     template = Template(filename=input_path + input_filename)
 
